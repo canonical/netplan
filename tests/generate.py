38b6f38bee5d1bd2efa8d90cd3fba224b78a4fb2
Martin Pitt <martin.pitt@ubuntu.com>
2016-07-26 16:28:22 +0200

    Add license/copyright headers to all source files

--- tests/generate.py
+++ tests/generate.py
@@ -2,6 +2,21 @@
 # Blackbox tests of netplan generate that verify that the generated
 # configuration files look as expected. These are run during "make check" and
 # don't touch the system configuration at all.
+#
+# Copyright (C) 2016 Canonical, Ltd.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
--- tests/integration.py
+++ tests/integration.py
@@ -3,6 +3,21 @@
 # started on the generated configuration, using emulated ethernets (veth) and
 # Wifi (mac80211-hwsim). These need to be run in a VM and do change the system
 # configuration.
+#
+# Copyright (C) 2016 Canonical, Ltd.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
