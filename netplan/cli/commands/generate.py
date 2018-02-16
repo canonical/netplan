f1d30fe307c0559885ddc3519435ed575799c740
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2018-02-15 22:09:23 -0500

    Fix class comment for generate.py

--- netplan/cli/commands/generate.py
+++ netplan/cli/commands/generate.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'''netplan ip command line'''
+'''netplan generate command line'''
 
 import logging
 import sys
