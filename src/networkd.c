canonical/netplan

commit ad0f8989ee5eab9aceb7ec5b35eadf8498046df8

Add output for optional=true in the networkd backend.

src/networkd.c
@@ -227,6 +227,9 @@ write_network_file(net_definition* def, const char* rootdir, const char* path)
     s = g_string_sized_new(200);
     append_match_section(def, s, TRUE);
 
+    if (def->optional)
+        g_string_append(s, "\n[Link]\nRequiredForOnline=yes\n");
+
     g_string_append(s, "\n[Network]\n");
     if (def->dhcp4 && def->dhcp6)
         g_string_append(s, "DHCP=yes\n");

tests/generate.py
@@ -277,16 +277,25 @@ class TestNetworkd(TestBase):
     '''networkd output'''
 
     def test_eth_optional(self):
-        # TODO: cyphermox: this is to validate that "optional" does not cause
-        #       any extra config to be generated; and will fail once it's actually
-        #       implemented.
         self.generate('''network:
   version: 2
   ethernets:
     eth0:
       dhcp6: true
       optional: true''')
-        self.assert_networkd({'eth0.network': ND_DHCP6 % 'eth0'})
+        self.assert_networkd({'eth0.network': '''[Match]
+Name=eth0
+
+[Link]
+RequiredForOnline=yes
+
+[Network]
+DHCP=ipv6
+
+[DHCP]
+UseMTU=true
+RouteMetric=100
+'''})
 
     def test_eth_wol(self):
         self.generate('''network:

commit f16b2a12e96139987d28bbcd4ffdb3cf920b7a03

networkd: fix optional=true output
RequiredForOnline should be no, not yes

Fixes: ad0f8989ee5e ("Add output for optional=true in the networkd backend.")
Signed-off-by: Daniel Axtens <dja@axtens.net>

src/networkd.c
@@ -234,7 +234,7 @@ write_network_file(net_definition* def, const char* rootdir, const char* path)
     append_match_section(def, s, TRUE);
 
     if (def->optional)
-        g_string_append(s, "\n[Link]\nRequiredForOnline=yes\n");
+        g_string_append(s, "\n[Link]\nRequiredForOnline=no\n");
 
     g_string_append(s, "\n[Network]\n");
     if (def->dhcp4 && def->dhcp6)

tests/generate.py
@@ -287,7 +287,7 @@ class TestNetworkd(TestBase):
 Name=eth0
 
 [Link]
-RequiredForOnline=yes
+RequiredForOnline=no
 
 [Network]
 DHCP=ipv6