2481a4ec55301c3d7927e9555b30ada0f926a138
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2019-08-14 10:09:28 -0400

    Add 'info' DBus method

--- src/dbus.c
+++ src/dbus.c
@@ -9,6 +9,8 @@
 #include <glib.h>
 #include <systemd/sd-bus.h>
 
+#include "_features.h"
+
 static int method_apply(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
     g_autoptr(GError) err = NULL;
     g_autofree gchar *stdout = NULL;
@@ -34,9 +36,60 @@ static int method_apply(sd_bus_message *m, void *userdata, sd_bus_error *ret_err
     return sd_bus_reply_method_return(m, "b", true);
 }
 
+static int method_info(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
+    sd_bus_message *reply = NULL;
+    g_autoptr(GError) err = NULL;
+    g_autofree gchar *stdout = NULL;
+    g_autofree gchar *stderr = NULL;
+    gint exit_status = 0;
+
+    exit_status = sd_bus_message_new_method_return(m, &reply);
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_open_container(reply, 'a', "(sv)");
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_append(reply, "(sv)", "Version", "s", NETPLAN_VERSION);
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_open_container(reply, 'r', "sv");
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_append(reply, "s", "Features");
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_open_container(reply, 'v', "as");
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_append_strv(reply, (char**)feature_flags);
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_close_container(reply);
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_close_container(reply);
+    if (exit_status < 0)
+       return exit_status;
+
+    exit_status = sd_bus_message_close_container(reply);
+    if (exit_status < 0)
+       return exit_status;
+
+    return sd_bus_send(NULL, reply, NULL);
+}
+
 static const sd_bus_vtable netplan_vtable[] = {
     SD_BUS_VTABLE_START(0),
     SD_BUS_METHOD("Apply", "", "b", method_apply, 0),
+    SD_BUS_METHOD("Info", "", "a(sv)", method_info, 0),
     SD_BUS_VTABLE_END
 };
 
