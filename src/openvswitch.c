71b64cef734edcd0e838bcadc8a18794fcb7f254
Lukas Märdian <lukas.maerdian@canonical.com>
2020-10-12 19:20:16 +0200

    Fix MAAS/OVS first boot for single NIC/PXE systems (#165)
    Netplan's Open vSwitch helper units are started too late, which makes systemd-networkd-wait-online.service fail the first time it is started. This leads to cloud-init init running in an invalid state, thus failing, which in turn makes the system (deployed via MAAS) end up with broken networking.
    
    The helper units only depend on OVSDB, which is started in network-pre.target, thus we can make them depend on ovsdb-server.service instead of openvswitch-switch.service, to avoid failures in wait-online.
    
    Commits:
    * openvswitch: start OVS units earlier
    openvswitch-switch.service runs pretty late and actually our service
    units only need access to the ovs-vsctl tool, which depends on
    ovsdb-server.service only. Depend on that earlier unit, to have OVS set
    up before networkd starts
    * ovs: do not run units Before=systemd-networkd.service

--- src/openvswitch.c
+++ src/openvswitch.c
@@ -38,8 +38,8 @@ write_ovs_systemd_unit(const char* id, const GString* cmds, const char* rootdir,
     g_string_append_printf(s, "Description=OpenVSwitch configuration for %s\n", id);
     g_string_append(s, "DefaultDependencies=no\n");
     /* run any ovs-netplan unit only after openvswitch-switch.service is ready */
-    g_string_append_printf(s, "Wants=openvswitch-switch.service\n");
-    g_string_append_printf(s, "After=openvswitch-switch.service\n");
+    g_string_append_printf(s, "Wants=ovsdb-server.service\n");
+    g_string_append_printf(s, "After=ovsdb-server.service\n");
     if (physical) {
         id_escaped = systemd_escape((char*) id);
         g_string_append_printf(s, "Requires=sys-subsystem-net-devices-%s.device\n", id_escaped);
