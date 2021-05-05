2eafea30931cfec3d31da06f4cdb0573b77cbec8
Lukas Märdian <slyon@ubuntu.com>
2021-05-05 11:15:12 +0200

    dbus:cli: implement io.netplan.Netplan.Generate() (#208)
    Proxy the 'netplan generate' CLI via DBus, if called from within a snap,
    to work around the strict AppArmor confinement.
    
    see: snapcore/snapd#10212
    
    COMMITS:
    * dbus:cli: implement io.netplan.Netplan.Generate()
    Proxy the 'netplan generate' CLI via DBus, if called from within a snap,
    to work around the strict AppArmor confinement.
    see: https://github.com/snapcore/snapd/pull/10212
    * doc: mention Netplan.Generate() DBus method

--- netplan/cli/commands/apply.py
+++ netplan/cli/commands/apply.py
@@ -80,6 +80,7 @@ class NetplanApply(utils.NetplanCommand):
             #      using core20 netplan binary/client/CLI on core18 base systems. Any change
             #      must be agreed upon with the snapd team, so we don't break support for
             #      base systems running older netplan versions.
+            #      https://github.com/snapcore/snapd/pull/5915
             res = subprocess.call([busctl, "call", "--quiet", "--system",
                                    "io.netplan.Netplan",  # the service
                                    "/io/netplan/Netplan",  # the object
