27a129cd35f725005efad0db75effe4621892a01
Lukas Märdian <slyon@ubuntu.com>
2022-01-12 11:10:33 +0100

    test:sriov: update _prepare_sysfs_dir_structure() to make it create PF/VF mocks dynamically

--- tests/test_sriov.py
+++ tests/test_sriov.py
@@ -68,41 +68,76 @@ class TestSRIOV(unittest.TestCase):
         os.makedirs(os.path.join(self.workdir.name, 'etc/netplan'))
         self.configmanager = ConfigManager(prefix=self.workdir.name, extra_files={})
 
-    def _prepare_sysfs_dir_structure(self):
+    def _prepare_sysfs_dir_structure(self, pf=('enp2', '0000:00:1f.0'),
+                                     vfs=[('enp2s16f1', '0000:00:1f.6')], pf_driver='dummy_driver'):
+        """
+        Setup sysfs mock for reading certain SR-IOV related files and symlinks
+
+        :param tuple pf: A tuple descibing the physical function (iterface_name, pci_address)
+        :param list vfs: A list of tuples describing the virtual functions related to this PF
+        :param str pf_driver: The driver name to be mocked for this PF
+        """
+        pf_iface, pf_pci_addr = pf
         # prepare a directory hierarchy for testing the matching
         # this might look really scary, but that's how sysfs presents devices
         # such as these
-        os.makedirs(os.path.join(self.workdir.name, 'sys/class/net'))
+        sysfs = os.path.join(self.workdir.name, 'sys')
+        sys_devices = os.path.join(sysfs, 'devices/pci0000:00')
+        pci_devices = os.path.join(sysfs, 'bus/pci/devices')
+        pci_driver = os.path.join(sysfs, 'bus/pci/drivers', pf_driver)
+        os.makedirs(os.path.join(sysfs, 'class/net'), exist_ok=True)
+        os.makedirs(pci_devices, exist_ok=True)
+        os.makedirs(pci_driver, exist_ok=True)  # access to 'bind' and 'unbind' files must be mocked
 
-        # first the VF
-        vf_iface_path = os.path.join(self.workdir.name, 'sys/devices/pci0000:00/0000:00:1f.6/net/enp2s16f1')
-        vf_dev_path = os.path.join(self.workdir.name, 'sys/devices/pci0000:00/0000:00:1f.6')
-        os.makedirs(vf_iface_path)
-        with open(os.path.join(vf_dev_path, 'vendor'), 'w') as f:
-            f.write('0x001f\n')
-        with open(os.path.join(vf_dev_path, 'device'), 'w') as f:
-            f.write('0xb33f\n')
-        os.symlink('../../devices/pci0000:00/0000:00:1f.6/net/enp2s16f1',
-                   os.path.join(self.workdir.name, 'sys/class/net/enp2s16f1'))
-        os.symlink('../../../0000:00:1f.6', os.path.join(self.workdir.name, 'sys/class/net/enp2s16f1/device'))
-
-        # now the PF
-        os.path.join(self.workdir.name, 'sys/class/net/enp2')
-        pf_iface_path = os.path.join(self.workdir.name, 'sys/devices/pci0000:00/0000:00:1f.0/net/enp2')
-        pf_dev_path = os.path.join(self.workdir.name, 'sys/devices/pci0000:00/0000:00:1f.0')
+        # create the PF (enp2) dir
+        # syfs mock in:
+        # sys/devices/pci0000:00/PCI_ADDR
+        # sys/devices/pci0000:00/PCI_ADDR/net/IFACE
+        pf_iface_path = os.path.join(sys_devices, pf_pci_addr, 'net', pf_iface)
+        pf_dev_path = os.path.join(sys_devices, pf_pci_addr)
         os.makedirs(pf_iface_path)
+        # symlink it to /sys/bus/pci/devices
+        os.symlink(os.path.join('../../../devices/pci0000:00', pf_pci_addr),
+                   os.path.join(pci_devices, pf_pci_addr))
+
+        # create VF (enp2s16f1, ...) dirs
+        # sysfs mock in:
+        # sys/devices/pci0000:00/PCI_ADDR and
+        # sys/devices/pci0000:00/PCI_ADDR/net/IFACE
+        for vf_iface, vf_pci_addr in vfs:
+            vf_iface_path = os.path.join(sys_devices, vf_pci_addr, 'net', vf_iface)
+            vf_dev_path = os.path.join(sys_devices, vf_pci_addr)
+            os.makedirs(vf_iface_path)
+            # symlink it to /sys/bus/pci/devices
+            os.symlink(os.path.join('../../../devices/pci0000:00', vf_pci_addr),
+                       os.path.join(pci_devices, vf_pci_addr))
+
+            # populate the VF data
+            with open(os.path.join(vf_dev_path, 'vendor'), 'w') as f:
+                f.write('0x001f\n')
+            with open(os.path.join(vf_dev_path, 'device'), 'w') as f:
+                f.write('0xb33f\n')
+            os.symlink(os.path.join('../../devices/pci0000:00', vf_pci_addr, 'net', vf_iface),
+                       os.path.join(sysfs, 'class/net', vf_iface))
+            os.symlink(os.path.join('../../..', vf_pci_addr),
+                       os.path.join(sysfs, 'class/net', vf_iface, 'device'))
+            # the VFs additionally have a device link to the PF
+            os.symlink(os.path.join('../../..', pf_pci_addr), os.path.join(vf_dev_path, 'physfn'))
+
+        # populate the PF data
         with open(os.path.join(pf_dev_path, 'vendor'), 'w') as f:
             f.write('0x001f\n')
         with open(os.path.join(pf_dev_path, 'device'), 'w') as f:
             f.write('0x1337\n')
-        os.symlink('../../devices/pci0000:00/0000:00:1f.0/net/enp2',
-                   os.path.join(self.workdir.name, 'sys/class/net/enp2'))
-        os.symlink('../../../0000:00:1f.0', os.path.join(self.workdir.name, 'sys/class/net/enp2/device'))
+        with open(os.path.join(pf_dev_path, 'sriov_numvfs'), 'w') as f:
+            f.write(str(len(vfs))+'\n')
+        os.symlink(os.path.join('../../../bus/pci/drivers', pf_driver), os.path.join(pf_dev_path, 'driver'))
+        os.symlink(os.path.join('../../devices/pci0000:00', pf_pci_addr, 'net', pf_iface),
+                   os.path.join(sysfs, 'class/net', pf_iface))
+        os.symlink(os.path.join('../../..', pf_pci_addr), os.path.join(sysfs, 'class/net', pf_iface, 'device'))
         # the PF additionally has device links to all the VFs defined for it
-        os.symlink('../../../0000:00:1f.4', os.path.join(pf_dev_path, 'virtfn0'))
-        os.symlink('../../../0000:00:1f.5', os.path.join(pf_dev_path, 'virtfn1'))
-        os.symlink('../../../0000:00:1f.6', os.path.join(pf_dev_path, 'virtfn2'))
-        os.symlink('../../../0000:00:1f.7', os.path.join(pf_dev_path, 'virtfn3'))
+        for i in range(len(vfs)):
+            os.symlink(os.path.join('../../..', vfs[i][1]), os.path.join(pf_dev_path, 'virtfn'+str(i)))
 
     @patch('netplan.cli.utils.get_interface_driver_name')
     @patch('netplan.cli.utils.get_interface_macaddress')
@@ -407,11 +442,14 @@ class TestSRIOV(unittest.TestCase):
         self.assertEqual(check_call.call_count, 1)
         self.assertListEqual(check_call.call_args[0][0],
                              ['ip', 'link', 'set', 'dev', 'enp2',
-                              'vf', '2', 'vlan', '10'])
+                              'vf', '0', 'vlan', '10'])
 
     @patch('subprocess.check_call')
     def test_apply_vlan_filter_for_vf_failed_no_index(self, check_call):
-        self._prepare_sysfs_dir_structure()
+        self._prepare_sysfs_dir_structure(vfs=[('enp2s14f1', '0000:00:1f.4'),
+                                               ('enp2s15f1', '0000:00:1f.5'),
+                                               ('enp2s16f1', '0000:00:1f.6'),
+                                               ('enp2s17f1', '0000:00:1f.7')])
         # we remove the PF -> VF link, simulating a system error
         os.unlink(os.path.join(self.workdir.name, 'sys/class/net/enp2/device/virtfn2'))
 
