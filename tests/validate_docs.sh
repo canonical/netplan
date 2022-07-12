d467c13fdaa6e1976862c9d23e4df39381bbdefb
Lukas Märdian <slyon@ubuntu.com>
2022-07-12 12:40:43 +0200

    tests: fix doc validation

--- tests/validate_docs.sh
+++ tests/validate_docs.sh
@@ -6,7 +6,7 @@
 # sanity check: make sure none have disappeared, as might happen from a reformat.
 count=$(sed -n 's/[ ]\+{"\([a-z0-9-]\+\)", YAML_[A-Z]\+_NODE.*/\1/p' src/parse.c | sort | wc -l)
 # 144 is based on 0.99+da6f776 definitions, and should be updated periodically.
-if [ $count -lt 144 ]; then
+if [ $count -lt 202 ]; then
     echo "ERROR: fewer YAML keys defined in src/parse.c than expected!"
     echo "       Has the file been reformatted or refactored? If so, modify"
     echo "       validate_docs.sh appropriately."
@@ -16,13 +16,13 @@ fi
 # iterate through the keys
 for term in $(sed -n 's/[ ]\+{"\([a-z0-9-]\+\)", YAML_[A-Z]\+_NODE.*/\1/p' src/parse.c | sort | uniq); do
     # it can be documented in the following ways.
-    # 1. "Properties for device type ``blah:``
-    if egrep "## Properties for device type \`\`$term:\`\`" doc/netplan-yaml.md > /dev/null; then
+    # 1. "Properties for device type `blah:`
+    if egrep "## Properties for device type \`$term:\`" doc/netplan-yaml.md > /dev/null; then
         continue
     fi
 
-    # 2. "[blah, ]``blah``[, ``blah2``]: (scalar|bool|...)
-    if egrep "\`\`$term\`\`.*\((scalar|bool|mapping|sequence of scalars|sequence of mappings|sequence of sequence of scalars)" doc/netplan-yaml.md > /dev/null; then
+    # 2. "[blah, ]**blah**[, **blah2**]: (scalar|bool|...)
+    if egrep "\*\*$term\*\*.*\((scalar|bool|mapping|sequence of scalars|sequence of mappings|sequence of sequence of scalars)" doc/netplan-yaml.md > /dev/null; then
         continue
     fi
 
