89948ed72b80c2671981ce6bb995dccbb5e389b6
Lukas Märdian <slyon@ubuntu.com>
2022-08-24 12:34:51 +0200

    buildsystem: meson fixes
    - install all files
    - POSIX shell compatibility in generators
    - use custom_targets for generators
    - fix dbus install directory

--- features_h_generator.sh
+++ features_h_generator.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-OUTPUT=src/_features.h
-INPUT=src/[^_]*.[hc]
+BASE=$(dirname $0)
+OUTPUT=$BASE/src/_features.h
+INPUT=$BASE/src/[!_]*.[hc]
 printf "#include <stddef.h>\nstatic const char *feature_flags[] __attribute__((__unused__)) = {\n" > $OUTPUT
 awk 'match ($0, /netplan-feature:.*/ ) { $0=substr($0, RSTART, RLENGTH); print "\""$2"\"," }' $INPUT >> $OUTPUT
 echo "NULL, };" >> $OUTPUT
+cat $OUTPUT
--- features_py_generator.sh
+++ features_py_generator.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-OUTPUT=netplan/_features.py
-INPUT=src/[^_]*.[hc]
+BASE=$(dirname $0)
+OUTPUT=$BASE/netplan/_features.py
+INPUT=$BASE/src/[!_]*.[hc]
 echo "# Generated file" > $OUTPUT
 echo "NETPLAN_FEATURE_FLAGS = [" >> $OUTPUT
 awk 'match ($0, /netplan-feature:.*/ ) { $0=substr($0, RSTART, RLENGTH); print "    \""$2"\"," }' $INPUT >> $OUTPUT
 echo "]" >> $OUTPUT
+cat $OUTPUT
