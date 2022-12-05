8e552082d80170e0c7a26605467e99d1c2e4c9a1
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2022-12-05 12:04:42 +0000

    parse: set filepath correctly after redefinition
    When the object is redefined by a different yaml file, the filepath must
    be updated to refer to that file.
    
    Add a unit test to check for this invariant.

--- src/parse.c
+++ src/parse.c
@@ -2839,6 +2839,12 @@ handle_network_ovs_settings_global_ports(NetplanParser* npp, yaml_node_t* node, 
             component1 = netplan_netdef_new(npp, scalar(port), NETPLAN_DEF_TYPE_PORT, NETPLAN_BACKEND_OVS);
             if (g_hash_table_remove(npp->missing_id, scalar(port)))
                 npp->missing_ids_found++;
+        }
+
+        if (npp->current.filepath) {
+            if (component->filepath)
+                g_free(component->filepath);
+
             component->filepath = g_strdup(npp->current.filepath);
         }
 
@@ -2852,6 +2858,12 @@ handle_network_ovs_settings_global_ports(NetplanParser* npp, yaml_node_t* node, 
             component2 = netplan_netdef_new(npp, scalar(peer), NETPLAN_DEF_TYPE_PORT, NETPLAN_BACKEND_OVS);
             if (g_hash_table_remove(npp->missing_id, scalar(peer)))
                 npp->missing_ids_found++;
+        }
+
+        if (npp->current.filepath) {
+            if (component->filepath)
+                g_free(component->filepath);
+
             component->filepath = g_strdup(npp->current.filepath);
         }
 
