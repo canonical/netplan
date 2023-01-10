39a12dc12e958d468b6cdf9631f5b60065f72a8b
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2023-01-10 10:15:01 +0000

    networkd: make sure VXLAN is in the right section
    If an interface has a list of routes and is linked to a vxlan, the
    VXLAN keyword is added to the [Route] section in the resulting networkd
    configuration file.
    
    This change makes the vxlan handling to happen before routes so VXLAN=
    will be added to the [Network] section.
    
    It fixes LP#2000713

--- tests/generator/test_tunnels.py
+++ tests/generator/test_tunnels.py
@@ -1684,3 +1684,45 @@ class TestConfigErrors(TestBase):
 '''
         out = self.generate(config, expect_fail=True)
         self.assertIn("Error in network definition: tun0: 'output-key' is not required for this tunnel type", out)
+
+    def test_vxlan_eth_with_route(self):
+        """[networkd] Validate that VXLAN= is in the right section (LP#2000713)"""
+        config = '''network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      routes:
+        - to: 10.20.30.40/32
+          via: 10.20.30.1
+  tunnels:
+    vxlan1:
+      mode: vxlan
+      id: 1
+      link: eth0
+'''
+        self.generate(config)
+        self.assert_networkd({'eth0.network': '''[Match]
+Name=eth0
+
+[Network]
+LinkLocalAddressing=ipv6
+VXLAN=vxlan1
+
+[Route]
+Destination=10.20.30.40/32
+Gateway=10.20.30.1
+''',
+                              'vxlan1.netdev': '''[NetDev]
+Name=vxlan1
+Kind=vxlan
+
+[VXLAN]
+VNI=1''',
+                              'vxlan1.network': '''[Match]
+Name=vxlan1
+
+[Network]
+LinkLocalAddressing=ipv6
+ConfigureWithoutCarrier=yes
+'''})
