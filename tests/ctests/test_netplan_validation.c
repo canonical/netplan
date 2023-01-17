31f3c7a2a86904e75a5a17661ca766a15f896e4d
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2023-01-17 12:10:52 +0000

    ctests: add tests for the new validation function
    Also add a utility to load the yaml content from strings.

---
+++ tests/ctests/test_netplan_validation.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+
+#include <cmocka.h>
+
+#include "netplan.h"
+#include "parse.h"
+
+#undef __USE_MISC
+#include "error.c"
+#include "names.c"
+#include "validation.c"
+#include "types.c"
+#include "util.c"
+#include "parse.c"
+
+#include "test_utils.h"
+
+void
+test_validate_interface_name_length(void** state)
+{
+    const char* yaml =
+        "network:\n"
+        "  version: 2\n"
+        "  bridges:\n"
+        "    ashortname:\n"
+        "      dhcp4: no\n";
+
+    NetplanState* np_state = load_string_to_netplan_state(yaml);
+    NetplanStateIterator iter;
+    NetplanNetDefinition* netdef = NULL;
+    netplan_state_iterator_init(np_state, &iter);
+
+    netdef = netplan_state_iterator_next(&iter);
+
+    assert_true(validate_interface_name_length(netdef));
+
+    netplan_state_clear(&np_state);
+}
+
+void
+test_validate_interface_name_length_set_name(void** state)
+{
+    const char* yaml =
+        "network:\n"
+        "  version: 2\n"
+        "  ethernets:\n"
+        "    eth0:\n"
+        "      match:\n"
+        "        macaddress: aa:bb:cc:dd:ee:ff\n"
+        "      set-name: ashortname\n";
+
+    NetplanState* np_state = load_string_to_netplan_state(yaml);
+    NetplanStateIterator iter;
+    NetplanNetDefinition* netdef = NULL;
+    netplan_state_iterator_init(np_state, &iter);
+
+    netdef = netplan_state_iterator_next(&iter);
+
+    assert_true(validate_interface_name_length(netdef));
+
+    netplan_state_clear(&np_state);
+}
+
+void
+test_validate_interface_name_length_too_long(void** state)
+{
+    const char* yaml =
+        "network:\n"
+        "  version: 2\n"
+        "  bridges:\n"
+        "    averylongnameforaninterface:\n"
+        "      dhcp4: no\n";
+
+    NetplanState* np_state = load_string_to_netplan_state(yaml);
+    NetplanStateIterator iter;
+    NetplanNetDefinition* netdef = NULL;
+    netplan_state_iterator_init(np_state, &iter);
+
+    netdef = netplan_state_iterator_next(&iter);
+
+    assert_false(validate_interface_name_length(netdef));
+
+    netplan_state_clear(&np_state);
+}
+
+void
+test_validate_interface_name_length_set_name_too_long(void** state)
+{
+    const char* yaml =
+        "network:\n"
+        "  version: 2\n"
+        "  ethernets:\n"
+        "    eth0:\n"
+        "      match:\n"
+        "        macaddress: aa:bb:cc:dd:ee:ff\n"
+        "      set-name: averylongnameforaninterface\n";
+
+    NetplanState* np_state = load_string_to_netplan_state(yaml);
+    NetplanStateIterator iter;
+    NetplanNetDefinition* netdef = NULL;
+    netplan_state_iterator_init(np_state, &iter);
+
+    netdef = netplan_state_iterator_next(&iter);
+
+    assert_false(validate_interface_name_length(netdef));
+
+    netplan_state_clear(&np_state);
+}
+
+int
+setup(void** state)
+{
+    return 0;
+}
+
+int
+tear_down(void** state)
+{
+    return 0;
+}
+
+int
+main()
+{
+
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_validate_interface_name_length),
+        cmocka_unit_test(test_validate_interface_name_length_too_long),
+        cmocka_unit_test(test_validate_interface_name_length_set_name),
+        cmocka_unit_test(test_validate_interface_name_length_set_name_too_long),
+    };
+
+    return cmocka_run_group_tests(tests, setup, tear_down);
+
+}
