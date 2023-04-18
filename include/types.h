8a6ebbbfb44faedde976519f42bb2cf90c3ce2a9
Danilo Egea Gondolfo <danilo.egea.gondolfo@canonical.com>
2023-04-18 17:34:13 +0100

    netplan: introduce some classes of errors
    Define Netplan specific errors so we don't need to use generic glib
    error codes.

--- include/types.h
+++ include/types.h
@@ -74,3 +74,65 @@ typedef struct _NetplanStateIterator NetplanStateIterator;
 struct _NetplanStateIterator {
     void* placeholder;
 };
+
+/*
+ * Errors and error domains
+ *
+ * NOTE: if new errors or domains are added,
+ * netplan/libnetplan.py must be updated with the new entries.
+ */
+
+enum NETPLAN_ERROR_DOMAINS {
+    NETPLAN_PARSER_ERROR = 1,
+    NETPLAN_VALIDATION_ERROR,
+    NETPLAN_FILE_ERROR,
+    NETPLAN_BACKEND_ERROR,
+    NETPLAN_EMITTER_ERROR,
+    NETPLAN_FORMAT_ERROR,
+};
+
+/*
+ * Errors for domain NETPLAN_PARSER_ERROR
+ *
+ * PARSER_ERRORS are expected to contain the file name, line and column numbers
+ */
+enum NETPLAN_PARSER_ERRORS {
+    NETPLAN_ERROR_INVALID_YAML,
+    NETPLAN_ERROR_INVALID_CONFIG
+};
+
+/*
+ * Errors for domain NETPLAN_VALIDATION_ERROR
+ *
+ * VALIDATION_ERRORS are expected to contain only the YAML file name
+ * where the error was found.
+ */
+enum NETPLAN_VALIDATION_ERRORS {
+    NETPLAN_ERROR_CONFIG_GENERIC,
+    NETPLAN_ERROR_CONFIG_VALIDATION,
+};
+
+/*
+ * Errors for domain NETPLAN_BACKEND_ERROR
+ */
+enum NETPLAN_BACKEND_ERRORS {
+    NETPLAN_ERROR_UNSUPPORTED,
+    NETPLAN_ERROR_VALIDATION,
+};
+
+/*
+ * Errors for domain NETPLAN_EMITTER_ERROR
+ */
+enum NETPLAN_EMITTER_ERRORS {
+    NETPLAN_ERROR_YAML_EMITTER,
+};
+
+/*
+ * Errors for domain NETPLAN_FORMAT_ERROR
+ *
+ * FORMAT_ERRORS are generic errors emitted from contexts where information
+ * like the file name is not known.
+ */
+enum NETPLAN_FORMAT_ERRORS {
+    NETPLAN_ERROR_FORMAT_INVALID_YAML,
+};
