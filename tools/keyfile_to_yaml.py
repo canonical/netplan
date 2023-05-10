59aabf9077057f89d40737c5c06e09cdd5badfd1
Danilo Egea Gondolfo <danilo.egea.gondolfo@canonical.com>
2023-05-10 16:23:38 +0100

    tools: add a tool to convert keyfiles to YAML
    It's handy during troubleshootings.

---
+++ tools/keyfile_to_yaml.py
@@ -0,0 +1,28 @@
+# A simple tool to convert a Network Manager keyfile to Netplan YAML
+# How to use:
+#   From the Netplan source directory, run:
+#     PYTHONPATH=. python3 tools/keyfile_to_yaml.py path/to/the/file.nmconnection
+
+import io
+import sys
+
+from netplan import libnetplan
+
+if len(sys.argv) < 2:
+    print("Pass the NM keyfile as parameter")
+    sys.exit(1)
+
+parser = libnetplan.Parser()
+state = libnetplan.State()
+
+try:
+    parser.load_keyfile(sys.argv[1])
+    state.import_parser_results(parser)
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+output = io.StringIO()
+state.dump_yaml(output)
+
+print(output.getvalue())
