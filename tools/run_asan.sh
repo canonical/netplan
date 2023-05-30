29656262ab2db349f136562d952e66c70eca10c8
Danilo Egea Gondolfo <danilo.egea.gondolfo@canonical.com>
2023-05-30 13:22:48 +0100

    tools/run_asan.sh: call the keyfile loader on NM files
    The script was changed to run the test with both networkd and
    NetworkManager renderers (when possible).
    For the NetworkManager keyfiles generated in the second step, it will
    call the new tool to load it.
    
    It will improve the coverage of the memory leak tests.

--- tools/run_asan.sh
+++ tools/run_asan.sh
@@ -4,9 +4,15 @@ set -e
 set -x
 
 BUILDDIR="_leakcheckbuild"
+CC=gcc
 
 meson setup ${BUILDDIR} -Db_sanitize=address,undefined
 meson compile -C ${BUILDDIR} --verbose
+
+${CC} tools/keyfile_to_yaml.c -o tools/keyfile_to_yaml \
+    -lnetplan $(pkg-config --cflags --libs glib-2.0) \
+    -Iinclude -L${BUILDDIR}/src \
+    -fsanitize=address,undefined -g
 
 TESTS=$(find ${BUILDDIR}/tests/ctests/ -executable -type f)
 for test in ${TESTS}
@@ -15,12 +21,36 @@ do
 done
 
 mkdir -p ${BUILDDIR}/fakeroot/{etc/netplan,run}
-export LD_LIBRARY_PATH="${BUILDDIR}/src"
 
 for yaml in examples/*.yaml
 do
+    filepath=${BUILDDIR}/fakeroot/etc/netplan/${yaml##*/}
+    filename=$(basename ${filepath})
     cp ${yaml} ${BUILDDIR}/fakeroot/etc/netplan/
-    chmod 600 ${BUILDDIR}/fakeroot/etc/netplan/${yaml##*/}
-    ./${BUILDDIR}/src/generate --root-dir ${BUILDDIR}/fakeroot
-    rm ${BUILDDIR}/fakeroot/etc/netplan/${yaml##*/}
+    chmod 600 ${filepath}
+
+    # Set the renderer and check if the new file can be parsed with the new renderer
+    # We use the system's netplan because it will not fail if there's a memory leak
+    netplan set --root-dir ${BUILDDIR}/fakeroot --origin-hint ${filename/.yaml/} network.renderer=networkd
+    if netplan generate --root-dir ${BUILDDIR}/fakeroot > /dev/null 2>&1
+    then
+        LD_LIBRARY_PATH="${BUILDDIR}/src" ./${BUILDDIR}/src/generate --root-dir ${BUILDDIR}/fakeroot
+    else
+        echo "File ${filename} can't be parsed with renderer = networkd"
+    fi
+
+    netplan set --root-dir ${BUILDDIR}/fakeroot --origin-hint ${filename/.yaml/} network.renderer=NetworkManager
+    if netplan generate --root-dir ${BUILDDIR}/fakeroot > /dev/null 2>&1
+    then
+        LD_LIBRARY_PATH="${BUILDDIR}/src" ./${BUILDDIR}/src/generate --root-dir ${BUILDDIR}/fakeroot
+        for keyfile in $(find ${BUILDDIR}/fakeroot/run/NetworkManager/system-connections/ -type f)
+        do
+            sed -i 's/\[connection\]/\[connection\]\nuuid=c87fb5fc-f607-45f3-8fcd-720b83a742e4/' "${keyfile}"
+            LD_LIBRARY_PATH="${BUILDDIR}/src" ./tools/keyfile_to_yaml "${keyfile}"
+        done
+    else
+        echo "File ${filename} can't be parsed with renderer = NetworkManager"
+    fi
+
+    rm ${filepath}
 done
