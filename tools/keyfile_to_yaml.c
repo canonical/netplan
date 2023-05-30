c8d46f5ab901d7eb9a3660d709131b683bafb24b
Danilo Egea Gondolfo <danilo.egea.gondolfo@canonical.com>
2023-05-30 12:55:15 +0100

    tools: add a C keyfile loader tool
    It will be mainly used to help catching memory leaks.

---
+++ tools/keyfile_to_yaml.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <types.h>
+#include <netplan.h>
+#include <parse-nm.h>
+#include <parse.h>
+#include <util.h>
+
+
+int main(int argc, char** argv) {
+    NetplanParser *npp;
+    NetplanState *np_state;
+    NetplanError* error = NULL;
+
+    if (argc < 2) return 1;
+
+    npp = netplan_parser_new();
+
+    netplan_parser_load_keyfile(npp, argv[1], &error);
+    if (error) goto exit_parser;
+
+    np_state = netplan_state_new();
+
+    netplan_state_import_parser_results(np_state, npp, &error);
+    if (!error) printf("keyfile %s loaded\n", argv[1]);
+
+    netplan_state_clear(&np_state);
+
+exit_parser:
+    netplan_parser_clear(&npp);
+    if (error) netplan_error_clear(&error);
+
+    return 0;
+}
