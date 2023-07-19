ea7dbeadcaebfab8648c10dade9fbe62adec8881
Danilo Egea Gondolfo <danilo.egea.gondolfo@canonical.com>
2023-07-19 14:30:13 +0100

    ctests: add a new cmocka test file for the NM backend
    The only test at the moment is related to a memory leak plugged in the
    function netplan_state_finish_nm_write() that happens when the state is
    empty.
    
    A couple of non-related functions were renamed so they will not collide
    with functions with the same name defined in another file.

---
+++ tests/ctests/test_netplan_nm.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+
+#include <cmocka.h>
+
+#include "netplan.h"
+
+#include "error.c"
+#include "names.c"
+#include "netplan.c"
+#include "validation.c"
+#include "types.c"
+#include "util.c"
+#include "parse.c"
+#include "nm.c"
+
+#include "test_utils.h"
+
+/* Trying to write an empty netplan_state should return True without
+* actually writing anything.
+*/
+void
+test_write_empty_state(__unused void** state)
+{
+    const char* yaml =
+        "network:\n"
+        "  version: 2\n"
+        "  renderer: NetworkManager\n";
+
+    NetplanState* np_state = load_string_to_netplan_state(yaml);
+
+    assert_true(netplan_state_finish_nm_write(np_state, NULL, NULL));
+
+    netplan_state_clear(&np_state);
+}
+
+
+int
+setup(__unused void** state)
+{
+    return 0;
+}
+
+int
+tear_down(__unused void** state)
+{
+    return 0;
+}
+
+int
+main()
+{
+
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_write_empty_state),
+    };
+
+    return cmocka_run_group_tests(tests, setup, tear_down);
+
+}
