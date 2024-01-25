69fe6a3c805842ccbd23aa384a90cc339f0efa8b
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2024-01-25 12:25:40 +0000

    sriov: move the udev logic to a service unit
    netplan apply will be called multiple times in parallel when the udev
    rules matches multiple device. That might cause problems during boot.
    
    Move the call to "netplan apply --sriov-only" to a service so it will be
    called just once for all the interfaces. The sriov-rebind service must
    wait for this new service to finish before start running.
    
    This change was tests with real hardware and appears to work fine.

--- src/sriov.c
+++ src/sriov.c
@@ -40,6 +40,7 @@ write_sriov_rebind_systemd_unit(GHashTable* pfs, const char* rootdir, GError** e
     GString* s = g_string_new("[Unit]\n");
     g_string_append(s, "Description=(Re-)bind SR-IOV Virtual Functions to their driver\n");
     g_string_append_printf(s, "After=network.target\n");
+    g_string_append_printf(s, "After=netplan-sriov-apply.service\n");
 
     /* Run after udev */
     g_hash_table_iter_init(&iter, pfs);
@@ -51,7 +52,7 @@ write_sriov_rebind_systemd_unit(GHashTable* pfs, const char* rootdir, GError** e
 
     g_string_append(s, "\n[Service]\nType=oneshot\n");
     g_string_truncate(interfaces, interfaces->len-1); /* cut trailing whitespace */
-    g_string_append_printf(s, "ExecStart=" SBINDIR "/netplan rebind %s\n", interfaces->str);
+    g_string_append_printf(s, "ExecStart=" SBINDIR "/netplan rebind --debug %s\n", interfaces->str);
 
     _netplan_g_string_free_to_file(s, rootdir, path, NULL);
     g_string_free(interfaces, TRUE);
@@ -60,7 +61,41 @@ write_sriov_rebind_systemd_unit(GHashTable* pfs, const char* rootdir, GError** e
     if (symlink(path, link) < 0 && errno != EEXIST) {
         // LCOV_EXCL_START
         g_set_error(error, NETPLAN_FILE_ERROR, errno,
-                    "failed to create enablement symlink: %m\n");
+                    "failed to create enablement symlink: %m");
+        return FALSE;
+        // LCOV_EXCL_STOP
+    }
+    return TRUE;
+}
+
+STATIC gboolean
+write_sriov_apply_systemd_unit(GHashTable* pfs, const char* rootdir, GError** error)
+{
+    g_autofree gchar* id_escaped = NULL;
+    g_autofree char* link = g_strjoin(NULL, rootdir ?: "", "/run/systemd/system/multi-user.target.wants/netplan-sriov-apply.service", NULL);
+    g_autofree char* path = g_strjoin(NULL, "/run/systemd/system/netplan-sriov-apply.service", NULL);
+    GHashTableIter iter;
+    gpointer key;
+
+    GString* s = g_string_new("[Unit]\n");
+    g_string_append(s, "Description=Apply SR-IOV configuration\n");
+    g_string_append_printf(s, "After=network.target\n");
+
+    g_hash_table_iter_init(&iter, pfs);
+    while (g_hash_table_iter_next (&iter, &key, NULL)) {
+        g_string_append_printf(s, "After=sys-subsystem-net-devices-%s.device\n", (gchar*) key);
+    }
+
+    g_string_append(s, "\n[Service]\nType=oneshot\n");
+    g_string_append_printf(s, "ExecStart=" SBINDIR "/netplan apply --sriov-only\n");
+
+    _netplan_g_string_free_to_file(s, rootdir, path, NULL);
+
+    _netplan_safe_mkdir_p_dir(link);
+    if (symlink(path, link) < 0 && errno != EEXIST) {
+        // LCOV_EXCL_START
+        g_set_error(error, G_MARKUP_ERROR, G_MARKUP_ERROR_INVALID_CONTENT,
+                    "failed to create enablement symlink: %m");
         return FALSE;
         // LCOV_EXCL_STOP
     }
@@ -80,6 +115,8 @@ netplan_state_finish_sriov_write(const NetplanState* np_state, const char* rootd
 
     if (np_state) {
         GHashTable* rebind_pfs = g_hash_table_new(g_str_hash, g_str_equal);
+        GHashTable* apply_pfs = g_hash_table_new(g_str_hash, g_str_equal);
+
         /* Find netdev interface names for SR-IOV PFs*/
         for (GList* iterator = np_state->netdefs_ordered; iterator; iterator = iterator->next) {
             def = (NetplanNetDefinition*) iterator->data;
@@ -90,6 +127,15 @@ netplan_state_finish_sriov_write(const NetplanState* np_state, const char* rootd
                     pf = def;
                 else if (def->sriov_link)
                     pf = def->sriov_link;
+
+                if (pf) {
+                    if (pf->set_name)
+                        g_hash_table_add(apply_pfs, pf->set_name);
+                    else if (!pf->has_match) /* netdef_id == interface name */
+                        g_hash_table_add(apply_pfs, pf->id);
+                    else
+                        g_warning("%s: Cannot determine SR-IOV PF interface name.", pf->id);
+                }
             }
 
             if (pf && pf->sriov_delay_virtual_functions_rebind) {
@@ -103,17 +149,33 @@ netplan_state_finish_sriov_write(const NetplanState* np_state, const char* rootd
                               pf->id);
             }
         }
-        if (g_hash_table_size(rebind_pfs) > 0) {
-            ret = write_sriov_rebind_systemd_unit(rebind_pfs, rootdir, NULL);
-        }
-        g_hash_table_destroy(rebind_pfs);
-    }
 
-    if (any_sriov) {
-        /* For now we execute apply --sriov-only everytime there is a new
-        SR-IOV device appearing, which is fine as it's relatively fast */
-        GString *udev_rule = g_string_new("ACTION==\"add\", SUBSYSTEM==\"net\", ATTRS{sriov_totalvfs}==\"?*\", RUN+=\"/usr/sbin/netplan apply --sriov-only\"\n");
-        _netplan_g_string_free_to_file(udev_rule, rootdir, "run/udev/rules.d/99-sriov-netplan-setup.rules", NULL);
+        if (any_sriov) {
+            ret = write_sriov_apply_systemd_unit(apply_pfs, rootdir, NULL);
+            if (!ret) {
+                // LCOV_EXCL_START
+                g_warning("netplan-sriov-apply.service cannot be created.");
+                goto error;
+                // LCOV_EXCL_STOP
+            }
+
+            /*
+             * The sriov-apply service will always be created (as long as there is any sr-iov configuration)
+             * and the sriov-rebind MUST only run after apply. As sriov-apply will always be there if sriov-rebind
+             * is present, using the After= dependency statement is enough (Requires= is not necessary).
+            */
+            if (g_hash_table_size(rebind_pfs) > 0) {
+                ret = write_sriov_rebind_systemd_unit(rebind_pfs, rootdir, NULL);
+                if (!ret)
+                // LCOV_EXCL_START
+                    g_warning("netplan-sriov-rebind.service cannot be created.");
+                // LCOV_EXCL_STOP
+            }
+        }
+
+error:
+        g_hash_table_destroy(rebind_pfs);
+        g_hash_table_destroy(apply_pfs);
     }
 
     return ret;
